48b8861f569a06233569e971f143d13e9c852cda
adol <adolsalamanca@gmail.com>
2023-07-18 20:08:16 +0200

    refactor to make code testable

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,20 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+#[cfg(test)]
+mod tests {
+    use crate::count_lines;
+
+    #[test]
+    fn count_lines_works_as_expected() {
+        let mut lines = Vec::new();
+        lines.push(String::from("first line"));
+        lines.push(String::from("second line"));
+        lines.push(String::from("third line"));
+
+        assert_eq!(3, count_lines(lines));
+    }
+}
 
 fn main() {
     let arg_type = std::env::args().nth(1).expect("no type was specified");
@@ -26,55 +40,59 @@ fn main() {
         }
     }
 
+    let mut result:u32 = 0;
+
     match arg_type.as_str() {
-        "-l" => count_lines(all_lines),
-        "--lines" => count_lines(all_lines),
-        "-c" => count_characters(all_lines),
-        "--characters" => count_characters(all_lines),
-        "-w" => count_words(all_lines),
-        "--words" => count_words(all_lines),
+        "-l" => result = count_lines(all_lines),
+        "--lines" => result = count_lines(all_lines),
+        "-c" => result = count_characters(all_lines),
+        "--characters" => result = count_characters(all_lines),
+        "-w" => result = count_words(all_lines),
+        "--words" => result = count_words(all_lines),
         _ => {}
     }
+
+    println!("{}", result);
 }
 
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
+fn read_lines<T>(filename: T) -> io::Result<io::Lines<io::BufReader<File>>>
+    where T: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-fn count_lines(all_lines: Vec<String>){
-    let mut num_of_lines = 0;
+fn count_lines(all_lines: Vec<String>) -> u32 {
+    let mut num_of_lines:u32 = 0;
 
     for _line in all_lines {
         num_of_lines += 1;
     }
 
-    println!("{}", num_of_lines);
+    return num_of_lines;
 }
 
-fn count_characters(all_lines: Vec<String>){
-    let mut num_of_chars = 0;
+fn count_characters(all_lines: Vec<String>) -> u32 {
+    let mut num_of_chars:u32 = 0;
 
     for line in all_lines {
-        num_of_chars += line.chars().count() + 1;
+        num_of_chars += line.chars().count() as u32 + 1;
     }
 
-    println!("{}", num_of_chars);
+    return num_of_chars;
 }
 
-fn count_words(all_lines: Vec<String>){
-    let mut num_of_words = 0;
+fn count_words(all_lines: Vec<String>) -> u32{
+    let mut num_of_words:u32 = 0;
 
     for line in all_lines {
         num_of_words += line.
             split(" ").
             filter(|w| (!w.eq_ignore_ascii_case(""))).
-            count()
+            count() as u32
     }
 
-    println!("{}", num_of_words);
+    return num_of_words;
 }
